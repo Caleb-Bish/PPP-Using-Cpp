64b520bf0fbda6453bc4a8f62ab8b46413998c89
Caleb-Bish <196969217+Caleb-Bish@users.noreply.github.com>
2025-08-18 14:23:25 +0100

    begun chapter 7

---
+++ Chapter_7/basic-declaration.cpp
@@ -0,0 +1,15 @@
+// The 'TRY THIS' section from Section 7.2.
+
+#include <iostream>
+
+// Stub function.
+int f(int v) {
+    return v;
+}
+
+
+int main() {
+    int i {7};
+    std::cout << f(i) << '\n';
+    return 0;
+}
